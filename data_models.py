4c6580ced8170c86418ede77b473d36d4fef74ce
mmt3 <42502920+0xphoton@users.noreply.github.com>
2022-08-01 14:20:33 -0700

    completed order DB, bug fixes, new features
    - added table management features to table_manager
    - bug fixes + error handling on reservoir_tools_client_v2
    - automatic detailed order creating (for database)

--- data_models.py
+++ data_models.py
@@ -3,23 +3,45 @@ from sqlalchemy import ARRAY, Column, Numeric, String
 class OrderModel():
     __tablename__ = "orders"
 
-    project_name = Column(String)
-    nft_id = Column(String)
-    currency = Column(String)
-    value = Column(Numeric)
-    marketplace = Column(String)
-    order_type = Column(String)
-    created_at = Column(String)
-    expires_on = Column(String)
+    project_name = Column('project_name', String)
+    nft_id = Column('nft_id', String)
+    currency = Column('currency', String)
+    value = Column('value', Numeric)
+    marketplace = Column('marketplace', String)
+    order_type = Column('order_type', String)
+    created_at = Column('created_at', String)
+    expires_on = Column('expires_on', String)
 
 class TradeModel():
     __tablename__ = "trades"
 
-    project_name = Column(String)
-    nft_id = Column(String)
-    currency = Column(String)
-    value = Column(Numeric)
-    marketplace = Column(String)
-    trade_data = Column(String)
-    Buyer = Column(String)
-    Seller = Column(String)
+    project_name = Column('project_name', String)
+    nft_id = Column('nft_id', String)
+    currency = Column('currency', String)
+    value = Column('value', Numeric)
+    marketplace = Column('marketplace', String)
+    trade_date = Column('order_type', String)
+    buyer = Column('buyer', String)
+    seller = Column('seller', String)
+
+class Order:
+    def __init__(self, project_name, nft_id, currency, value, marketplace, order_type, created_at, expires_on):
+        self.project_name = project_name
+        self.nft_id = nft_id
+        self.currency = currency
+        self.value = value
+        self.marketplace = marketplace
+        self.order_type = order_type
+        self.created_at = created_at
+        self.expires_on = expires_on
+
+class Trade:
+    def __init__(self, project_name, nft_id, currency, value, marketplace, trade_data, buyer, seller):
+        self.project_name = project_name
+        self.nft_id = nft_id
+        self.currency = currency
+        self.value = value
+        self.marketplace = marketplace
+        self.trade_data = trade_data
+        self.buyer = buyer
+        self.seller = seller
